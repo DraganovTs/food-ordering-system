2a6257877590b8ae3586f18f2c78fc1828e9f80e
DraganovTs <cvetomir_draganov@abv.bg>
2023-11-04 15:15:21 +0200

    order data access impl

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.food.ordering.system.order.service.dataaccess.restaurant.adapter;
+
+import com.food.ordering.system.order.service.dataaccess.restaurant.entity.RestaurantEntity;
+import com.food.ordering.system.order.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
+import com.food.ordering.system.order.service.dataaccess.restaurant.repository.RestaurantJpaRepository;
+import com.food.ordering.system.order.service.domain.ports.output.repository.RestaurantRepository;
+import com.food.ordering.system.order.system.domain.entity.Restaurant;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class RestaurantRepositoryImpl implements RestaurantRepository {
+
+    private final RestaurantJpaRepository restaurantJpaRepository;
+    private final RestaurantDataAccessMapper restaurantDataAccessMapper;
+
+    public RestaurantRepositoryImpl(RestaurantJpaRepository restaurantRepository,
+                                    RestaurantDataAccessMapper restaurantDataAccessMapper) {
+        this.restaurantJpaRepository = restaurantRepository;
+        this.restaurantDataAccessMapper = restaurantDataAccessMapper;
+    }
+
+    @Override
+    public Optional<Restaurant> findRestaurantInformation(Restaurant restaurant) {
+        List<UUID> restaurantProducts = restaurantDataAccessMapper.restaurantToRestaurantProducts(restaurant);
+        Optional<List<RestaurantEntity>> restaurantEntities = restaurantJpaRepository
+                .findByRestaurantIdAndProductIdIn(restaurant.getId().getValue(),
+                        restaurantProducts);
+        return restaurantEntities.map(restaurantDataAccessMapper::restaurantEntityToRestaurant);
+    }
+}
