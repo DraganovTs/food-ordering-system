46448e5dd9987ec8db3ab5acdf001f7b0bc51069
DraganovTs <cvetomir_draganov@abv.bg>
2023-11-12 09:19:03 +0200

    refactor order service for outbox pattern

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderPaymentSaga.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderPaymentSaga.java
@@ -1,52 +1,76 @@
 package com.food.ordering.system.order.service.domain;
 
-import com.food.ordering.system.domain.event.EmptyEvent;
 import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.outbox.model.payment.OrderPaymentOutboxMessage;
+import com.food.ordering.system.order.service.domain.outbox.sheduler.payment.PaymentOutboxHelper;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
 import com.food.ordering.system.order.system.domain.OrderDomainService;
 import com.food.ordering.system.order.system.domain.entity.Order;
 import com.food.ordering.system.order.system.domain.event.OrderPaidEvent;
+import com.food.ordering.system.saga.SagaStatus;
 import com.food.ordering.system.saga.SagaStep;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Slf4j
 @Component
-public class OrderPaymentSaga implements SagaStep<PaymentResponse, OrderPaidEvent, EmptyEvent> {
+public class OrderPaymentSaga implements SagaStep<PaymentResponse> {
 
     private final OrderDomainService orderDomainService;
     private final OrderSagaHelper orderSagaHelper;
-    private final OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher;
+    private final PaymentOutboxHelper paymentOutboxHelper;
+    private final OrderRepository orderRepository;
 
     public OrderPaymentSaga(OrderDomainService orderDomainService,
                             OrderSagaHelper orderSagaHelper,
-                            OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher) {
+                            PaymentOutboxHelper paymentOutboxHelper,
+                            OrderRepository orderRepository) {
         this.orderDomainService = orderDomainService;
         this.orderSagaHelper = orderSagaHelper;
-        this.orderPaidRestaurantRequestMessagePublisher = orderPaidRestaurantRequestMessagePublisher;
+        this.paymentOutboxHelper = paymentOutboxHelper;
+        this.orderRepository = orderRepository;
     }
 
     @Override
     @Transactional
-    public OrderPaidEvent process(PaymentResponse paymentResponse) {
+    public void process(PaymentResponse paymentResponse) {
+
+        Optional<OrderPaymentOutboxMessage> orderPaymentOutboxMessageResponse =
+                paymentOutboxHelper.getPaymentOutboxMessageBySagaIdAndSagaStatus(
+                        UUID.fromString(paymentResponse.getSagaId()),
+                        SagaStatus.STARTED);
+
+        if (orderPaymentOutboxMessageResponse.isEmpty()) {
+            log.info("An outbox message with saga id: {} is already processed!", paymentResponse.getSagaId());
+            return;
+        }
+
+        OrderPaymentOutboxMessage orderPaymentOutboxMessage = orderPaymentOutboxMessageResponse.get();
+
         log.info("Completing payment dor order with id: {}", paymentResponse.getOrderId());
         Order order = orderSagaHelper.findOrder(paymentResponse.getOrderId());
-        OrderPaidEvent domainEvent = orderDomainService.payOrder(order, orderPaidRestaurantRequestMessagePublisher);
+        OrderPaidEvent domainEvent = orderDomainService.payOrder(order);
         orderSagaHelper.saveOrder(order);
+
+        SagaStatus sagaStatus = orderSagaHelper.orderStatusToSagaStatus(domainEvent.getOrder().getOrderStatus());
+
         log.info("Order with id: {} is paid", order.getId().getValue());
-        return domainEvent;
+
     }
 
 
     @Override
     @Transactional
-    public EmptyEvent rollback(PaymentResponse paymentResponse) {
+    public void rollback(PaymentResponse paymentResponse) {
         log.info("Cancelling payment dor order with id: {}", paymentResponse.getOrderId());
         Order order = orderSagaHelper.findOrder(paymentResponse.getOrderId());
         orderDomainService.cancelOrder(order, paymentResponse.getFailureMessages());
         orderSagaHelper.saveOrder(order);
         log.info("Order with id: {} is cancelled", order.getId().getValue());
-        return EmptyEvent.INSTANCE;
     }
 
 
